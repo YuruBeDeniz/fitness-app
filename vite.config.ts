51d6f4a2ddebe2c01a6d55a2c6301b2598b49ae6
Deniz Saganak <saganak.deniz@gmail.com>
2023-02-21 17:23:19 +0100

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from "path"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: "@",  replacement: path.resolve(__dirname, "src")}]
+  }
+})
+
+//find @ and replace it with current directory folder (__dirname)
+//and add src to it. 
