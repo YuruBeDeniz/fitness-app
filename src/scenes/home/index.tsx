f0caf0d55cc6bfd5bc29b3f792cf4c10b2b6e1a5
Deniz Saganak <saganak.deniz@gmail.com>
2023-03-15 09:37:12 +0100

    finish home page

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -15,7 +15,7 @@ type Props = {
 }
 
 const Home = ({ setSelectedPage }: Props) => {
-  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
 
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
@@ -24,7 +24,16 @@ const Home = ({ setSelectedPage }: Props) => {
       {/* MAIN HEADER */}
       <div className="z-10 mt-32 md:basis-3/5">
         {/* HEADINGS */}
-        <div className="md:-mt-20">
+        <motion.div 
+          initial="hidden" 
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 }
+          }}
+          className="md:-mt-20">
           <div className="relative">
             <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
                 <img alt="home-page-text" src={HomePageText} />
@@ -35,7 +44,7 @@ const Home = ({ setSelectedPage }: Props) => {
             Studios to get the Body Shapes That you Dream of.. Get Your Dream
             Body Now.
           </p>
-        </div>
+        </motion.div>
       </div>
       
       {/* ACTIONS */}
@@ -53,18 +62,21 @@ const Home = ({ setSelectedPage }: Props) => {
       </div>
 
       {/* IMAGE */}
-      <div className="flex basis-3/5 justify-center md:z-10">
+      <div className="flex basis-3/5 justify-center md:z-10
+            md:ml-40 md:mt-16 md: justify-items-end">
         <img src={HomePageGraphic} alt="home-pageGraphic"/>
       </div>
      </div>
 
      {/* SPONSORS */}
      {isAboveMediumScreens && 
-      <div>
-        <div>
+      <div className="h-[150px] w-full bg-primary-100 py-10">
+        <div className="mx-auto w-5/6">
+         <div className="flex w-3/5 justify-between gap-8"> 
           <img src={SponsorRedBull} />
           <img src={SponsorForbes} />
           <img src={SponsorFortune} />
+          </div> 
         </div>
       </div> 
      }
